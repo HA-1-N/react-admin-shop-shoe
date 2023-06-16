e029c50c33ea99b78eddb372c887caa9db3673ec
HA-1-N <hoanganh02062001@gmail.com>
2023-06-16 16:40:32 +0700

    feat(color): call api color

--- src/pages/Addcolor.js
+++ src/pages/Addcolor.js
@@ -1,7 +1,7 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import CustomInput from "../components/CustomInput";
 import { useDispatch, useSelector } from "react-redux";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import * as yup from "yup";
 import { useFormik } from "formik";
@@ -11,85 +11,127 @@ import {
   resetState,
   updateAColor,
 } from "../features/color/colorSlice";
+import {
+  createColorApi,
+  getColorByCodeApi,
+  updateColorApi,
+} from "../api/color.api";
 let schema = yup.object().shape({
-  title: yup.string().required("Color is Required"),
+  colorCode: yup.string().required("colorCode is Required"),
+  colorName: yup.string().required("colorName is Required"),
 });
 const Addcolor = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-  const getColorId = location.pathname.split("/")[3];
-  const newColor = useSelector((state) => state.color);
-  const {
-    isSuccess,
-    isError,
-    isLoading,
-    createdColor,
-    updatedColor,
-    colorName,
-  } = newColor;
+  const params = useParams();
+  const colorId = params.id;
+  const colorCodeLocation = location.state?.colorCode;
+
+  const [colorCodeDetail, setColorCodeDetail] = useState("");
+  const [colorNameDetail, setColorNameDetail] = useState("");
+
   useEffect(() => {
-    if (getColorId !== undefined) {
-      dispatch(getAColor(getColorId));
+    if (colorId !== undefined) {
+      const data = {
+        colorCode: colorCodeLocation,
+      };
+      getColorByCodeApi(data).then((res) => {
+        const data = res?.data?.data;
+        setColorCodeDetail(data?.colorCode);
+        setColorNameDetail(data?.colorName);
+      });
     } else {
       dispatch(resetState());
     }
-  }, [getColorId]);
-  useEffect(() => {
-    if (isSuccess && createdColor) {
-      toast.success("Color Added Successfullly!");
-    }
-    if (isSuccess && updatedColor) {
-      toast.success("Color Updated Successfullly!");
-      navigate("/admin/list-color");
-    }
-    if (isError) {
-      toast.error("Something Went Wrong!");
-    }
-  }, [isSuccess, isError, isLoading, createdColor]);
+  }, [colorId]);
+
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {
-      title: colorName || "",
+      colorCode: colorCodeDetail || "",
+      colorName: colorNameDetail || "",
     },
     validationSchema: schema,
     onSubmit: (values) => {
-      if (getColorId !== undefined) {
-        const data = { id: getColorId, colorData: values };
-        dispatch(updateAColor(data));
-        dispatch(resetState());
+      if (colorId !== undefined) {
+        const data = {
+          ...values,
+        };
+        updateColorApi(data, colorId)
+          .then((res) => {
+            if (res) {
+              formik.resetForm();
+              setTimeout(() => {
+                dispatch(resetState());
+              }, 300);
+              navigate("/admin/list-color");
+              toast.success("Update Color successful !");
+            } else {
+              toast.error("Error !");
+            }
+          })
+          .catch((err) => {
+            toast.error(err);
+          });
       } else {
-        dispatch(createColor(values));
-        formik.resetForm();
-        setTimeout(() => {
-          dispatch(resetState());
-        }, 300);
+        const data = {
+          ...values,
+        };
+        createColorApi(data, colorId)
+          .then((res) => {
+            if (res) {
+              formik.resetForm();
+              setTimeout(() => {
+                dispatch(resetState());
+              }, 300);
+              navigate("/admin/list-color");
+              toast.success("Create Color successful !");
+            } else {
+              toast.error("Error !");
+            }
+          })
+          .catch((err) => {
+            toast.error(err);
+          });
       }
     },
   });
   return (
     <div>
       <h3 className="mb-4 title">
-        {getColorId !== undefined ? "Edit" : "Add"} Color
+        {colorId !== undefined ? "Edit" : "Add"} Color
       </h3>
       <div>
         <form action="" onSubmit={formik.handleSubmit}>
           <CustomInput
             type="color"
-            label="Enter Product Color"
-            onChng={formik.handleChange("title")}
-            onBlr={formik.handleBlur("title")}
-            val={formik.values.title}
-            id="color"
+            label="Enter Product Color Code"
+            onChng={formik.handleChange("colorCode")}
+            onBlr={formik.handleBlur("colorCode")}
+            val={formik.values.colorCode}
+            id="colorCode"
           />
           <div className="error">
-            {formik.touched.title && formik.errors.title}
+            {formik.touched.colorCode && formik.errors.colorCode}
+          </div>
+
+          <CustomInput
+            type="text"
+            label="Enter Product Color"
+            onChng={formik.handleChange("colorName")}
+            onBlr={formik.handleBlur("colorName")}
+            val={formik.values.colorName}
+            id="colorName"
+          />
+          <div className="error">
+            {formik.touched.colorName && formik.errors.colorName}
           </div>
           <button
             className="btn btn-success border-0 rounded-3 my-5"
             type="submit"
           >
-            {getColorId !== undefined ? "Edit" : "Add"} Color
+            {colorId !== undefined ? "Edit" : "Add"} Color
           </button>
         </form>
       </div>
--- src/pages/Colotlist.js
+++ src/pages/Colotlist.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteAColor, getColors } from "../features/color/colorSlice";
+import {
+  deleteAColor,
+  getColors,
+  resetState,
+} from "../features/color/colorSlice";
 import { BiEdit } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CustomModal from "../components/CustomModal";
+import { PAGE_SIZE } from "../constants/page.constants";
+import { deleteColorApi, filterColorApi } from "../api/color.api";
+import { toast } from "react-toastify";
+import { Box, Button, Pagination } from "@mui/material";
 
 const columns = [
   {
     title: "SNo",
     dataIndex: "key",
+  },
+  {
+    title: "Code",
+    dataIndex: "colorCode",
   },
   {
     title: "Name",
@@ -23,37 +35,68 @@ const columns = [
 ];
 
 const Colorlist = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const [open, setOpen] = useState(false);
   const [colorId, setcolorId] = useState("");
-  const showModal = (e) => {
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+  const [colorDetail, setColorDetail] = useState([]);
+  const [colorItem, setColorItem] = useState(null);
+
+  const showModal = (item) => {
     setOpen(true);
-    setcolorId(e);
+    setcolorId(item?._id);
+    setColorItem(item);
   };
 
   const hideModal = () => {
     setOpen(false);
   };
-  const dispatch = useDispatch();
+
+  const getColorDetail = async () => {
+    const data = {
+      colorCode: "",
+      colorName: "",
+    };
+    const params = {
+      page: page,
+      limit: PAGE_SIZE,
+    };
+    filterColorApi(data, params)
+      .then((res) => {
+        setColorDetail(res?.data?.data);
+        setTotalPage(res?.data?.pagination?.totalPages);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   useEffect(() => {
-    dispatch(getColors());
+    dispatch(resetState());
+    getColorDetail();
   }, []);
-  const colorState = useSelector((state) => state.color.colors);
+
   const data1 = [];
-  for (let i = 0; i < colorState.length; i++) {
+  for (let i = 0; i < colorDetail.length; i++) {
     data1.push({
       key: i + 1,
-      name: colorState[i].title,
+      colorCode: colorDetail[i].colorCode,
+      name: colorDetail[i].colorName,
       action: (
         <>
           <Link
-            to={`/admin/color/${colorState[i]._id}`}
+            to={`/admin/color/${colorDetail[i]._id}`}
+            state={{ colorCode: colorDetail[i]?.colorCode }}
             className=" fs-3 text-danger"
           >
             <BiEdit />
           </Link>
           <button
             className="ms-3 fs-3 text-danger bg-transparent border-0"
-            onClick={() => showModal(colorState[i]._id)}
+            onClick={() => showModal(colorDetail[i])}
           >
             <AiFillDelete />
           </button>
@@ -61,19 +104,57 @@ const Colorlist = () => {
       ),
     });
   }
-  const deleteColor = (e) => {
-    dispatch(deleteAColor(e));
 
-    setOpen(false);
-    setTimeout(() => {
-      dispatch(getColors());
-    }, 100);
+  const handleClickBtnAddColor = () => {
+    navigate("/admin/color");
   };
+
+  const deleteColor = (e) => {
+    const data = {
+      colorCode: colorItem?.colorCode,
+    };
+
+    deleteColorApi(data)
+      .then((res) => {
+        if (res) {
+          setOpen(false);
+          setTimeout(() => {
+            getColorDetail();
+          }, 100);
+          toast.success("Delete brand successful");
+        } else {
+          toast.error("Error");
+        }
+      })
+      .catch((err) => {
+        toast.error(err);
+      });
+  };
+
+  const onChangePage = (e, pageNumber) => {
+    setPage(pageNumber);
+  };
+
   return (
     <div>
-      <h3 className="mb-4 title">Colors</h3>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h3 className="mb-4 title">Colors</h3>
+        <Button
+          sx={{ marginBottom: "1.5rem" }}
+          onClick={handleClickBtnAddColor}
+        >
+          Add color
+        </Button>
+      </Box>
       <div>
         <Table columns={columns} dataSource={data1} />
+        <Pagination page={page} count={totalPage} onChange={onChangePage} />
       </div>
       <CustomModal
         hideModal={hideModal}
