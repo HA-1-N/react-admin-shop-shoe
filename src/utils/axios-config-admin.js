7bf51164fe9a91d5c83a00a3428e4bb6f7508611
HA-1-N <hoanganh02062001@gmail.com>
2023-09-21 21:50:43 +0700

    add host server

--- src/utils/axios-config-admin.js
+++ src/utils/axios-config-admin.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = "https://api-shop-shoe.onrender.com/api";
+const BASE_URL = "https://13.228.225.19/api";
 // const BASE_URL = "http://localhost:5000/api";
 
+// 13.228.225.19
+// 18.142.128.26
+// 54.254.162.138
 const getUser = localStorage.getItem("user");
 const user = JSON.parse(getUser);
 
